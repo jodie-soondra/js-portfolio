212bc44d3509ecb11100c2d6e2f47cbccc9a37e9
jodie-soondra <jodie.soondra@gmail.com>
2023-04-14 13:04:00 +1000

    conversion from vite to no vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      darkBlue: "#031322",
+      blue: "#051A2B",
+      brightOrange: "#FD4A05",
+      pastelOrange: "#F46843",
+      cream: "#FEE8DB",
+    },
+    fontFamily: {
+      sans: ["Helvetica"],
+    },
+    display:["group-hover"],
+  },
+  screens: {
+    xs: "480px",
+    ss: "620px",
+    sm: "768px",
+    md: "1060px",
+    lg: "1200px",
+    xl: "1700px",
+  },
   },
   plugins: [],
 }
